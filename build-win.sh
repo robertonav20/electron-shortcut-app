66eb0a432f3a59a6e92e3623091c1ce025653289
robertonav20 <roberto.nav20@gmail.com>
2025-05-30 08:08:54 +0000

    Fix windows build

--- build-win.sh
+++ build-win.sh
@@ -10,6 +10,6 @@ npm install --platform=win32
 yarn run package-win
 
 # Copy prebuilt binary lib instead to build manually
-cp lib/node_sqlite3.node out/Shortcut App-win32-x64/resources/app/.webpack/main/native_modules/build/Release
+cp lib/node_sqlite3.node out/"Shortcut App-win32-x64"/resources/app/.webpack/main/native_modules/build/Release
 
 yarn run make-win
